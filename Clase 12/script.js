595b803828fb60c0803b75d4839a2f5e70251fa6
juacax <mateos.joaquin@gmail.com>
2024-03-26 22:39:15 -0300

    Clase 13

--- Clase 12/script.js
+++ Clase 12/script.js
@@ -1,16 +1,19 @@
+let personajes = []
+
 async function buscarPersonajes() {
   const nombre = document.getElementById('inputBusqueda').value
   const div = document.getElementById('divResultado')
   const boton = document.getElementById('btnBusqueda')
 
   div.innerHTML = ''
+  personajes = []
+
   boton.innerHTML = `  
   <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
   <span role="status">Buscando...</span>
   `
   boton.disabled = true
 
-  let personajes = []
   let nextPage = ''
 
   const url = `https://rickandmortyapi.com/api/character/?name=${nombre}`
@@ -35,22 +38,61 @@ async function buscarPersonajes() {
 
   for (let i = 0; i < personajes.length; i++) {
     const p = personajes[i]
-    div.innerHTML += `<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-2">
-    <div class="card p-2" style="width: 18rem;">
-        <img src="${p.image}" class="card-img-top" alt="${p.name}">
-        <div class="card-body">
-            <h5 class="card-title">${p.name}</h5>
-            <p class="card-text">
-                <b>Estado: </b>${p.status}<br/>
-                <b>Tipo: </b>${p.type}<br/>
-                <b>Genero: </b>${p.gender}<br/>
-            </p>
-        </div>
-    </div>
-    
+    div.innerHTML += `
+    <div class="col-12 col-sm-6 col-md-4 col-xl-3 mb-2">
+      <div class="card p-2" style="width: 18rem;">
+          <img src="${p.image}" class="card-img-top" alt="${p.name}" >
+          <div class="card-img-overlay"  onclick="abrirModal(${i})" role="button">
+              <h5 class="card-title text-white text-center rounded" style="background-color:rgba(0,0,0,0.5)">${p.name}</h5>
+          </div>
+      </div>
     </div>`
   }
 
   boton.innerHTML = 'Buscar'
   boton.disabled = false
+}
+
+function abrirModal(id) {
+  console.log(personajes[id])
+  const p = personajes[id]
+  //document.getElementById('modalTitle').innerText = p.name
+
+  document.getElementById('modalBody').innerHTML = `
+  <div class="card ">
+  <div class="row g-0">
+    <div class="col-md-6">
+      <img src="${p.image}" class="img-fluid rounded-start" width=500 alt="${
+    p.name
+  }">
+    </div>
+    <div class="col-md-6">
+      <div class="card-body">
+        <h4>${p.name}</h4>
+        <p class="card-text"><b>Estado: </b>${
+          p.status == 'Alive'
+            ? '<span class="badge rounded-pill text-bg-success">Vivo</span>'
+            : p.status == 'Dead'
+            ? '<span class="badge rounded-pill text-bg-danger">Muerto</span>'
+            : '<span class="badge rounded-pill text-bg-secondary">' +
+              p.status +
+              '</span>'
+        }</p>
+        ${
+          p.type != ''
+            ? '<p class="card-text"><b>Tipo: </b>' + p.type + '</p>'
+            : ''
+        }
+        <p class="card-text"><b>Genero: </b>${p.gender}</p>
+        <p class="card-text"><b>Especie: </b>${p.species}</p>
+        <p class="card-text"><b>Origen: </b>${p.origin.name}</p>
+        <p class="card-text"><b>Ubicación: </b>${p.location.name}</p>
+
+      </div>
+    </div>
+  </div>
+</div>
+`
+
+  document.getElementById('btnModal').click()
 }
